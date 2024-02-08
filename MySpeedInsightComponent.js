aed7073e4c63138f85092b7faed624f375891b4a
XOwlPost - Resurgence Advocate <XOfidelius@proton.me>
2024-02-08 05:37:10 +0100

    Add MySpeedInsightComponent and update dependencies

---
+++ MySpeedInsightComponent.js
@@ -0,0 +1,11 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+const MySpeedInsightComponent = () => {
+  return (
+    <div>
+      <SpeedInsights url="https://21xo.com" />
+    </div>
+  );
+};
+
+export default MySpeedInsightComponent;
