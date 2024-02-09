db46a08905e39d136486c898daef325db45a1a4c
XOwlPost - Resurgence Advocate <XOfidelius@proton.me>
2024-02-09 01:59:32 +0100

    Add main script to package.json and import MySpeedInsightComponent in index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import MySpeedInsightComponent from '../components/MySpeedInsightComponent'; // Adjust the import path based on your file structure
+
+export default function HomePage() {
+  return (
+    <div>
+      {/* Your page content */}
+      <MySpeedInsightComponent />
+    </div>
+  );
+}
